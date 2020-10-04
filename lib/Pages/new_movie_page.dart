efafc35ae098bfe182a7b711f16a092b2b836590
ArnobMahmud <arnob.tech.me@gmail.com>
2020-10-04 19:38:59 +0600

    Form Validation

--- lib/Pages/new_movie_page.dart
+++ lib/Pages/new_movie_page.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
-import 'package:movie_stream_app/db/temp_db.dart';
 import 'package:movie_stream_app/models/movie_model.dart';
 
 class NewMoviePage extends StatefulWidget {
@@ -10,136 +8,92 @@ class NewMoviePage extends StatefulWidget {
 
 class _NewMoviePageState extends State<NewMoviePage> {
   var movie = Movie();
-  DateTime _selectedDate;
-  String category = 'Action';
-
   final _formKey = GlobalKey<FormState>();
   void _saveMovie() {
     if (_formKey.currentState.validate()) {
       _formKey.currentState.save();
-      movie.category = category;
     }
-  }
-
-  void _openDatePickerDialouge() async {
-    DateTime date = await showDatePicker(
-        context: context,
-        initialDate: DateTime.now(),
-        firstDate: DateTime(1940),
-        lastDate: DateTime(2030));
-
-    setState(() {
-      _selectedDate = date; //dd/mm/yyyy
-      movie.releaseYear = _selectedDate.year;
-    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Add New Movie'),
+        title: Text("Add New Movie"),
       ),
       body: Form(
-          key: _formKey,
-          child: Padding(
-            padding: const EdgeInsets.all(12.0),
-            child: ListView(
-              children: [
-                TextFormField(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListView(
+            children: [
+              TextFormField(
                   decoration: InputDecoration(
-                    labelText: 'Enter Movie Name',
+                    labelText: "Enter Movie Name",
                     border: OutlineInputBorder(),
                   ),
                   validator: (value) {
                     if (value.isEmpty) {
-                      return 'This Field Must Not be Empty';
+                      return "This field mustn't be empty";
                     }
                     return null;
                   },
                   onSaved: (value) {
                     movie.name = value;
-                  },
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                TextFormField(
+                  }),
+              SizedBox(
+                height: 10,
+              ),
+              TextFormField(
                   keyboardType: TextInputType.number,
                   decoration: InputDecoration(
-                    labelText: 'Enter Rating',
+                    labelText: "Enter Rating",
                     border: OutlineInputBorder(),
                   ),
                   validator: (value) {
                     if (value.isEmpty) {
-                      return 'This Field Must Not be Empty';
+                      return "This field mustn't be empty";
                     }
                     if (double.parse(value) < 0 || double.parse(value) > 10) {
-                      return 'Rating Should be Between 1 to 10.';
+                      return "TRating must be between 0 to 10";
                     }
                     return null;
                   },
                   onSaved: (value) {
                     movie.rating = double.parse(value);
-                  },
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                TextFormField(
-                  maxLines: 5,
+                  }),
+              SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                  maxLines: 6,
                   decoration: InputDecoration(
-                    labelText: 'Enter Description',
+                    labelText: "Enter Description",
                     border: OutlineInputBorder(),
                   ),
                   validator: (value) {
                     if (value.isEmpty) {
-                      return 'This Field Must Not be Empty';
+                      return "This field mustn't be empty";
                     }
                     return null;
                   },
                   onSaved: (value) {
-                    movie.name = value;
-                  },
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Row(
-                  children: [
-                    FlatButton(
-                        child: Text('Select Release Date'),
-                        onPressed: () {
-                          _openDatePickerDialouge();
-                          Text(_selectedDate == null
-                              ? 'Pick a date'
-                              : DateFormat('EEE, MMM dd , yyyy')
-                                  .format(_selectedDate));
-                        }),
-                  ],
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                DropdownButton(
-                    value: category,
-                    items: categories.map((cat) =>
-                        DropdownMenuItem(value: cat, child: Text(cat))),
-                    onChanged: (value) {
-                      setState(() {
-                        category = value;
-                      });
-                    }),
-                RaisedButton(
-                    textColor: Colors.white,
-                    color: Colors.red,
-                    child: Text('Save'),
-                    onPressed: () {
-                      _saveMovie();
-                    })
-              ],
-            ),
-          )),
+                    movie.description = value;
+                  }),
+              SizedBox(
+                height: 10,
+              ),
+              RaisedButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(17)),
+                color: Colors.red[200],
+                child: Text("Save"),
+                onPressed: _saveMovie,
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
